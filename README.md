889319db8e8a96e4f4a51cb1bd8361d9f2c22352
OneOverCosine <TheCandyDragon@gmail.com>
2021-03-23 15:43:48 +0000

    Added control_flow.py and updated README.md

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 - if, else, elif
 - for loops
 - while loops
+
+## Conditional Statements
+Use conditionals to run code you only want to execute under certain conditions. They are used to control the flow of your programme.
+```python
+age = 15 # if the user is above the age of 15 allow them to buy a ticket
+
+if age > 15:
+    print("You purchased a ticket!")
+elif age <= 15:
+    print("You're too young to buy a ticket.")
+else:
+    print("Something went wrong...") # this will execute if none of the other conditions are met
+
+```
+Outputs ``You're too young to buy a ticket.``
+
+## Loops!
+tba
---
+++ control_flow.py
@@ -0,0 +1,15 @@
+age = int(input("Enter your age: "))
+# if the user is above the age of 15 allow them to buy a ticket
+
+ratings = ['U', 'PG', '12', '15', '18']
+
+if age >= 18:
+    print(f"At {age}, you can watch moives reated {ratings[4]} and lower.")
+elif age >= 15:
+    print(f"At {age}, you can watch movies rated {ratings[3]} and lower.")
+elif age >= 12:
+    print(f"At {age}, you can watch movies rated {ratings[2]} lower.")
+elif age < 12:
+    print(f"At {age}, you can watch moives rated {ratings[1]}, but it's reccomended that an adult accompany you.\nYou can watch movies rated {ratings[0]} without accompaniment.")
+else:
+    print("Something went wrong...") # this will execute if none of the other conditions are met
